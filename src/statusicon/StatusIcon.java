803ded639e054e1fa1d79dc3e4142b0c30297065
redpois0n <red@cmail.nu>
2015-05-11 21:24:33 +0200

    Always progressIcons

--- src/statusicon/StatusIcon.java
+++ src/statusicon/StatusIcon.java
@@ -4,6 +4,7 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.TrayIcon;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.ImageIcon;
@@ -15,7 +16,7 @@ public class StatusIcon {
 	private TrayIcon icon;
 	private int max = 100;
 	private int value;
-	private List<Image> progressIcons;
+	private List<Image> progressIcons = new ArrayList<Image>();
 	
 	public StatusIcon(int width, int height, TrayIcon icon, List<Image> progressIcons) {
 		this.width = width;
@@ -25,11 +26,11 @@ public class StatusIcon {
 	}
 	
 	public StatusIcon(int width, int height, TrayIcon icon) {
-		this(width, height, icon, null);
+		this(width, height, icon, new ArrayList<Image>());
 	}
 	
 	public StatusIcon(TrayIcon icon) {
-		this(16, 16, icon, null);
+		this(16, 16, icon, new ArrayList<Image>());
 	}
 	
 	public StatusIcon() {
@@ -43,15 +44,19 @@ public class StatusIcon {
 	public BufferedImage update() {
 		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g = image.createGraphics();
+
+		Image current = progressIcons.get((int) (((float) value / (float) max) * progressIcons.size()));
+		g.drawImage(current, 0, 0, width, height, null);
 		
-		if (progressIcons == null) {
-			
-		} else {
-			Image current = progressIcons.get((int) (((float) value / (float) max) * progressIcons.size()));
-			g.drawImage(current, 0, 0, width, height, null);
+		if (icon != null) {
+			icon.setImage(image);
 		}
 		
 		return image;
+	}
+	
+	public List<Image> getIcons() {
+		return this.progressIcons;
 	}
 	
 	public int getValue() {
@@ -60,6 +65,7 @@ public class StatusIcon {
 	
 	public void setValue(int value) {
 		this.value = value;
+		update();
 	}
 	
 	public int getMaximum() {
